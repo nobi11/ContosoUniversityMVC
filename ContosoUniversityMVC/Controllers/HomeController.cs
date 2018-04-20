775248186584504588c9ee94e42bfaa397207f01
nanette.obien <Nanette.Obien@tattsgroup.com>
2018-04-20 13:53:40 +1000

    First Commit

---
+++ ContosoUniversityMVC/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using ContosoUniversityMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversityMVC.Data;
+using ContosoUniversityMVC.Models.SchoolViewModels;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversityMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public HomeController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> About()
+        {
+            //ViewData["Message"] = "Your application description page.";
+            //return View();
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ContosoUniversityMVC/Models/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversityMVC.Models
+{
+    public class Student : Person
+    {
+        //public int ID { get; set; }
+
+        //[Required]
+        //[DisplayName("Last Name")]
+        //[StringLength(50)]
+        //public string LastName { get; set; }
+
+        //[Required]
+        //[DisplayName("First Name")]
+        //[StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        //public string FirstMidName { get; set; }
+
+        [DisplayName("Enrollment Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime EnrollmentDate { get; set; }
+
+        //[Display(Name = "Full Name")]
+        //public string FullName
+        //{
+        //    get
+        //    {
+        //        return LastName + ", " + FirstMidName;
+        //    }
+        //}
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
